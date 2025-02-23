6deb429347f29fed2fa65ea4e1bf1137a23fcca6
Leonardo Kenji <leokk06@gmail.com>
2025-02-23 12:41:59 -0300

    Run tpm installation script

--- .scripts/install_scripts/tmux.sh
+++ .scripts/install_scripts/tmux.sh
@@ -1,1 +1,8 @@
+#!/usr/bin/env bash
+
 git clone https://github.com/tmux-plugins/tpm ${HOME}/.config/tmux/plugins/tpm
+
+# The install_plugins script expects the PWD to be this.
+cd ${HOME}/.config/tmux/plugins/tpm/bin
+
+${HOME}/.config/tmux/plugins/tpm/bin/install_plugins
