8acf115bda8c0c985d23fc259a2afed85d69adaf
Leonardo Kenji <leokk06@gmail.com>
2025-12-08 12:00:40 -0300

    Update rofi launch script for Rofi 2.0 in wayland with the required workaround for Gnome

---
+++ .scripts/launch_rofi.bash
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+set -Eeuo pipefail
+
+# Logging and error handling.
+
+SCRIPT_NAME="$(basename "${0}")"
+LOG_FILE="${HOME}/${SCRIPT_NAME}.log"
+
+# Redirect all stderr while still showing it.
+exec 2> >(tee -a "${LOG_FILE}" >&2)
+
+log() {
+  echo "[$(date '+%Y-%m-%d %H:%M:%S')] ${*}" | tee -a "${LOG_FILE}"
+}
+
+trap 'log "[Error] on line ${LINENO}: \"${BASH_COMMAND}\" (exit code: ${?})"; exit 1' ERR
+
+# Environment variables.
+
+# Force Xwayland as GNOME does not support the layer shell protocol required
+# for Rofi 2.0.
+export WAYLAND_DISPLAY=""
+
+# Flags
+
+FLAGS=(
+  # Run Rofi with a permanent cache folder (this is the history file, no idea
+  # why its considered cache).
+  -cache-dir "${HOME}/.config/rofi"
+  # Flags for running Rofi in Wayland (Looks like they are not necessary
+  # anymore).
+  -normal-window -steal-focus
+  # Just want this mode shown.
+  -show combi
+)
+
+rofi "${FLAGS[@]}"
--- .scripts/launch_rofi.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-# Run rofi with a permanent cache folder
-# Flags for running rofi in wayland
-rofi -show combi -normal-window -steal-focus -cache-dir ${HOME}/.config/rofi
